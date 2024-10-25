a1064ab717fd057491592f47daebd43d7ff651e3
Administrator <Administrator@DESKTOP-IH2TBC9>
2024-10-25 16:14:12 +0530

    tns

---
+++ day1program/src/com/tnsif/dayone/IfDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.dayone;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 6;
+		if (x != 5)
+		System.out.println("Value of x is not 5");
+	}
+
+}
