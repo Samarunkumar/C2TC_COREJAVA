adae1101ffa2e18e014fada1926950abe912a1a1
Administrator <Administrator@DESKTOP-IH2TBC9>
2024-10-25 15:13:00 +0530

    second program

---
+++ day1program/src/com/tnsif/dayone/Identifire.java
@@ -0,0 +1,12 @@
+package com.tnsif.dayone;
+
+public class Identifire {
+  public static void main(String[]args) {
+	  int $number = 20;
+	  System.out.println("Value of the number variable is : "+ $number);
+	  String studentName = "Praveen";
+	  System.out.println("Value is :"+studentName);
+  }
+	
+
+}
