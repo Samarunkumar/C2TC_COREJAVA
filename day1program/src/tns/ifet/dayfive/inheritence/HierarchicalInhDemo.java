cfaa163e345e9ec730cc653b6d55ef60a1524fa9
Administrator <Administrator@DESKTOP-IH2TBC9>
2024-11-05 05:49:16 +0530

    day5

--- day1program/src/tns/ifet/dayfive/inheritence/HierarchicalInhDemo.java
+++ day1program/src/tns/ifet/dayfive/inheritence/HierarchicalInhDemo.java
@@ -14,12 +14,12 @@ public class HierarchicalInhDemo {
 		if (p instanceof Person)
 			System.out.println("Person Details "+p);
 		
-		p = new Employee("Nikhil", "Mumbai", 101, 67000, "Sales");
+		p = new Person("Nikhil", "Mumbai", 101, 67000, "Sales");
 		if (p instanceof Employee)
 		System.out.println("Employee Details "+p);
 		
 
-		p = new Student("Pankaj", "Pune", "FE", 88);
+		p = new Person("Pankaj", "Pune", "FE", 88);
 		if (p instanceof Student)
 			System.out.println("Student Details "+p);
 
