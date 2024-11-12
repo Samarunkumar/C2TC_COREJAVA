8b83d94fc5bcc10844506c595c3811106211aec4
Administrator <Administrator@DESKTOP-IH2TBC9>
2024-11-12 15:21:52 +0530

    exception

---
+++ day1program/src/com/tns/ifet/day6/staticblockmethod/MyclassDemo.java
@@ -0,0 +1,23 @@
+package com.tns.ifet.day6.staticblockmethod;
+
+public class MyclassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Line");
+		MyClass o1 = new MyClass();
+		System.out.println(o1);
+
+		// static method with class name
+		MyClass.display();
+
+		MyClass o2 = new MyClass();
+		System.out.println(o2);
+		MyClass.display();
+		MyClass o3 = new MyClass();
+		System.out.println(o3);
+		MyClass.display();
+
+	}
+
+}
